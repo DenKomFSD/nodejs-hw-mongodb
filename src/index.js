25af2c87f690a8cb47a75120939e3faae48fc23f
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-16 13:02:52 +0300

    postman status 200

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-// src/index.js
+import express from 'express';
 
-const message = "Hello world";
+const PORT = 3000;
+const app = express();
 
-console.log(message);
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
