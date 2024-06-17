2382a9b85746415fcc36ed28bc98caa37fac8906
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-17 15:19:41 +0300

    env dotenv +examle

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+  //adding cors
+  app.use(cors());
+
+  app.use(express.json());
+
+  //pino logging request for formetting(put on the very begginning of middlewares)
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  //request
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  //middleware for request that is doesnt exist(adding in the end)
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'not found',
+    });
+    next();
+  });
+  //middleware with err
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  //listening server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
