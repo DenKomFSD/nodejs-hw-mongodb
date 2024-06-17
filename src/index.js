9e478762eeb631752c05ef31896d4f35ae86fc6c
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-17 14:03:31 +0300

    postman status 404 + 500

--- src/index.js
+++ src/index.js
@@ -2,13 +2,36 @@ import express from 'express';
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
+//middlware with showing time of request
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
 
+//request
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello world!',
   });
 });
 
-app.listen(PORT, (req, res) => {
+//middleware with err
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
+  });
+});
+
+//middleware for request that is doesnt exist(adding in the end)
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'not found',
+  });
+  next();
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
