27a0d82b81034903c407a3c1782f611a01ac9d0f
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-23 13:26:21 +0300

    stp 1 ready

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -17,47 +17,9 @@ export const setupServer = () => {
       target: 'pino-pretty',
     },
   });
+  app.use(contactsRouter);
   app.use(logger);
 
-  //request
-
-  app.get('/contacts', async (req, res) => {
-    const result = await getContacts();
-    res.json({
-      status: 200,
-      data: result,
-      message: 'Successfully get contacts',
-    });
-  });
-
-  //request by ID
-  app.get('/contacts/:contactId', async (req, res) => {
-    try {
-      const { contactId } = req.params;
-
-      const data = await getContactById(contactId);
-
-      if (!data) {
-        return res.status(404).json({
-          message: `Contact  not found`,
-        });
-      }
-      res.json({
-        status: 200,
-        data,
-        message: `Successfully found contact with id ${contactId}!`,
-      });
-    } catch (error) {
-      if (error.message.includes('Cast to ObjectId failed ')) {
-        error.status = 404;
-      }
-      const { status = 500 } = error;
-      res.status(status).json({
-        message: 'Something went wrong...',
-        error: error.message,
-      });
-    }
-  });
   //middleware for request that is doesnt exist(adding in the end)
   app.use('*', (req, res) => {
     res.status(404).json({
