c6162286683ac617c7e913915b7d1308a4b58a58
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-06-23 15:35:13 +0300

    minor improvements method get 1.1.0

---
+++ src/controllers/contacts.js
@@ -0,0 +1,41 @@
+import { getContacts, getContactById } from '../services/contacts.js';
+import createHttpError from 'http-errors';
+
+export const getAllContactsController = async (req, res, next) => {
+  try {
+    const result = await getContacts();
+    res.json({
+      status: 200,
+      data: result,
+      message: 'Successfully get contacts',
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const data = await getContactById(contactId);
+    //if NOT FOUND
+    if (!data) {
+      next(createHttpError(404, 'Contact not found'));
+      return;
+    }
+    res.json({
+      status: 200,
+      data,
+      message: `Successfully found contact with id ${contactId}!`,
+    });
+  } catch (error) {
+    if (error.message.includes('Cast to ObjectId failed ')) {
+      error.status = 404;
+    }
+    const { status = 500 } = error;
+    res.status(status).json({
+      message: 'Something went wrong',
+      data: { message: 'Contact not found' },
+    });
+  }
+};
