803899fb28343b22d941cbf61234f3ee4554e8c9
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-07-01 16:09:35 +0300

    hw4-validation

---
+++ src/validation/contacts-schemas.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+import {
+  phoneRegExp,
+  mailRegExp,
+  typeList,
+} from '../constants/contacts-constants.js';
+
+export const contactAddSchema = Joi.object({
+  name: Joi.string().required().min(3).max(20),
+  phoneNumber: Joi.string().required().pattern(phoneRegExp).min(3).max(20),
+  email: Joi.string().pattern(mailRegExp),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid(...typeList)
+    .default('personal'),
+});
