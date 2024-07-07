6a7bb1d82504d581621dd244758ae6f827e711ea
Denis <122015463+iziKomSD@users.noreply.github.com>
2024-07-07 18:00:17 +0300

    hw5-auth hash

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,13 @@
+import bcrypt from 'bcrypt';
+
 import User from '../db/models/User.js';
 
 export const findUser = (filter) => User.findOne(filter);
 
-export const signup = async (data) => User.create(data);
+export const signup = async (data) => {
+  const { password } = data;
+  //хешуємо пароль
+  const hashPassword = await bcrypt.hash(password, 10);
+  //розпилюєм і вертаєм не оригінальний пароль а захешований
+  return User.create({ ...data, password: hashPassword });
+};
